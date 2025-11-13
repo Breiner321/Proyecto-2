f40dd3db7a2137e90aa84ecc42e37249cdbb3e8a
Breiner321 <breialex523@gmail.com>
2025-11-13 15:19:58 -0500

    Configuración de BD
    Configuración de BD

--- MVCSampleFinal/Test/DomainTest/obj/Debug/net9.0/DomainTest.AssemblyInfo.cs
+++ MVCSampleFinal/Test/DomainTest/obj/Debug/net9.0/DomainTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ba6f0da49dc12035761dd9d75c203fbbcacef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MVCSampleFinal/Web/MvcSample/obj/Debug/net9.0/MvcSample.AssemblyInfo.cs
+++ MVCSampleFinal/Web/MvcSample/obj/Debug/net9.0/MvcSample.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ba6f0da49dc12035761dd9d75c203fbbcacef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
